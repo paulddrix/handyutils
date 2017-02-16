09e7798254e1d4965590ab8aee1a78d4624fa6e5
paulddrix <paulddrix@protonmail.com>
2017-02-16 12:11:39 -0500

    implementing isEmpty and debugNLog methods of the HandyUtils class

--- index.js
+++ index.js
@@ -1,41 +1,88 @@
-/**
- * @function debug
- * @param {string} title
- * Title to display on the debug output
- * @param {object} obj
- * Object will be displayed in the debug output
- * @example
- * handyutils.debug('The title of what is being tested', { objectToBeTested: true });
- */
-exports.debug = (title, obj) => {
-  if (process.env.DEBUG) {
-    console.log('\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n',
-      title,
-      '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n',
-      obj,
-      '\n===========================================\n\n');
+const fs = require('fs');
+class HandyUtils {
+  constructor(logDirPath) {
+    this.logsDir = logDirPath;
   }
-};
-
-/**
- * @function debug
- * @param {anydatatype} variable
- * Any data type
- * @example
- * handyutils.isEmpty({ objectToBeTested: true });
- */
-exports.isEmpty = (variable) => {
-  if (process.env.DEBUG) {
-    // if the variable is a string
-    if (typeof variable === string) {
-      // if the string is empty of underfined
-      if (variable === '' || variable === undefined){
-        console.log('EMPTY');
-        return true;
-      } else {
-        console.log('NOT EMPTY');
-        return false;
+  /**
+   * @function debug
+   * @param {string} title
+   * Title to display on the debug output
+   * @param {anydatatype} anydatatype
+   * anydatatype will be displayed in the debug output
+   * @example
+   * handyutils.debug('The title of what is being tested', { objectToBeTested: true });
+   */
+  static debug(title, anydatatype=''){
+    if (process.env.DEBUG) {
+      console.log('\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n',
+        title,
+        '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n',
+        anydatatype,
+        '\n===========================================\n\n');
+    }
+  }
+  /**
+   * @function isEmpty
+   * @param {anydatatype} variable
+   * Any data type
+   * @example
+   * handyutils.isEmpty({ objectToBeTested: true });
+   */
+  static isEmpty(variable){
+    if (process.env.DEBUG) {
+      switch (variable) {
+        case typeof variable === string:
+        // if the string is empty or underfined
+        if (variable === '' || variable === undefined){
+          console.log('String is empty');
+          return true;
+          break;
+        } else {
+          console.log('String is not empty');
+          return false;
+          break;
+        }
+        case typeof variable === object:
+        // if the object is empty or underfined
+          for(var key in variable) {
+            if(variable.hasOwnProperty(key))
+                return false;
+                break;
+            }
+          return true;
+          break;
+          case typeof variable === array:
+          // if the array is empty or underfined
+          if (variable.length ===0 || variable === undefined) {
+            return true;
+            break;
+          } else {
+            return false;
+            break;
+          }
       }
     }
   }
-};
+  /**
+   * @function debugNLog
+   * @param {anydatatype} variable
+   * Any data type
+   * @example
+   * handyutils.debugNLog('debugging and logging to file',{ objectToBeTested: true });
+   */
+   static debugNLog(title, anydatatype='', logType){
+     let output = '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n';
+     output += title;
+     output += '\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n';
+     output += anydatatype;
+     output += '\n===========================================\n\n';
+     if (process.env.DEBUG) {
+       console.log(output);
+     } else if(process.env.LOG_MODE) {
+       fs.writeFile(`${logType}.txt`, output, 'utf8', (err)=>{
+         if (err) throw err;
+       });
+     }
+   }
+}
+module.exports = HandyUtils;
